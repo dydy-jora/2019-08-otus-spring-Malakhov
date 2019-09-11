bba39957376b88377609540654adba1cc9add74a
Alexander <mavar66@mail.ru>
2019-09-11 22:33:27 +0300

    qaz

---
+++ lesson-01/src/main/java/tst/ls_01/HomeWork01.java
@@ -0,0 +1,18 @@
+package tst.ls_01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+
+public interface HomeWork01 {
+    String askQuestion(String qv);
+
+    void printResult(String fName, String lName);
+
+    String readRes(Resource res);
+
+    void startSurvey(String qv);
+
+    String getFileData();
+
+    void runme(ApplicationContext ctx);
+}
